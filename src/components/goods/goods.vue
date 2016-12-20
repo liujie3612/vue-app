de44eb1242355870dcfacb1828b133a71dadd308
liujie3612 <liujie@wilddog.com>
2016-12-20 21:11:53 +0800

    123

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
 	<div class="goods">
 		<div class="menu-wrapper" v-el:menu-wrapper>
 			<ul>
-				<li v-for='item in goods' class="menu-item" >
+				<li v-for='item in goods' class="menu-item" :class="{'current':currentIndex === $index}" @click="selectMenu($index,$event)">
 					<span class="text border-1px">
 						<span v-show='item.type>0' class="icon" :class="classMap[item.type]" ></span>{{item.name}}
 					</span>
@@ -11,7 +11,7 @@
 		</div>	
  		<div class="foods-wrapper" v-el:foods-wrapper>
  			<ul>
- 				<li v-for='item in goods' class="food-list">
+ 				<li v-for='item in goods' class="food-list food-list-hook">
  					<h1 class="title">{{item.name}}</h1>
  					<ul>
  						<li v-for='food in item.foods' class="food-item border-1px">
@@ -22,12 +22,10 @@
  								<h2 class="name">{{food.name}}</h2>
  								<p class="desc">{{food.description}}</p>
  								<div class="extra">
- 									<span class="count">月售{{food.sellCount}}份</span>
- 									<span>好评率{{food.rating}}%</span>
+ 									<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
  								</div>
  								<div class="price">
- 									<span class="now">￥{{food.price}}</span>
- 									<span class="old" v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
+ 									<span class="now">￥{{food.price}}</span><span class="old" v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
  								</div>
  							</div>
  						</li>
@@ -39,7 +37,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-	import BScroll from 'better-scroll'
+	import BScroll from 'better-scroll';
+
 	const ERR_OK = 0
 	export default{
 		prop:{
@@ -49,25 +48,67 @@
 		},
 		data() {
 			return {
-				goods:[]
+				goods:[],
+				listHeight: [],
+				scrollY: 0
 			}
 		},
 		created() {
+			this.classMap = ['decrease','discount','special','invoice','guarantee']
       		this.$http.get('/api/goods').then((response) => {
 		        response = response.body;
 		        if( response.errno === ERR_OK ) {
 		            this.goods = response.data;
-		            this._initScroll()
+		            this.$nextTick(() => {
+		            	this._initScroll()
+		            	this._calculateHeight()
+		            })  
 		        }
-      		});
-
-      		this.classMap = ['decrease','discount','special','invoice','guarantee']
+      		})
+    	},
+    	computed: {
+    		currentIndex() {
+    			for(let i = 0;i < this.listHeight.length; i++) {
+    				let height1 = this.listHeight[i]
+    				let height2 = this.listHeight[i + 1]
+    				if(!height2  || (this.scrollY >= height1 && this.scrollY < height2)) {
+    					return i;
+    				}
+    			}
+    			return 0
+    		}
     	},
     	methods: {
     		_initScroll() {
-    			this.menuScroll = new BScroll(this.$els.menusWrapper, {})
-    			this.foodsScroll = new BScroll(this.$els.foodsWrapper, {})
-    			console.log(this.menuScroll)
+    			this.menuScroll = new BScroll(this.$els.menuWrapper, {
+    				click : true
+    			});
+    			this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+    				probeType: 3
+    			})
+
+    			this.foodsScroll.on('scroll',(pos) => {
+    				this.scrollY = Math.abs(Math.round(pos.y))
+    			})
+    		},
+    		_calculateHeight() {
+    			let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+    			let height = 0
+    			this.listHeight.push(height)
+    			for(let i = 0;i < foodList.length; i++) {
+    				let item = foodList[i]
+    				height += item.clientHeight
+    				this.listHeight.push(height)
+    			}
+    		},
+    		selectMenu(index,event) {
+    			//过滤掉浏览器原生的click事件，只执行自定义的点击事件  pc页面的时候，派发了两次click事件 浏览器原生的点击事件没有_constructed属性
+    			if(!event._constructed) {
+    				return
+    			}
+    			let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+    			let el = foodList[index]
+    			this.foodsScroll.scrollToElement(el,300)
     		}
     	}
 	}
@@ -94,6 +135,15 @@
 				line-height: 16px
 				padding: 0 12px
 				letter-spacing: 1px;
+				&.current
+					z-index: 10
+					background: #fff
+					position: relative
+					margin-top: -1px
+					font-weight: 700
+					
+					.text
+						border-none()
 				.icon
 					display:inline-block
 					margin-bottom: -1px
@@ -123,12 +173,12 @@
 			flex: 1
 			.title
 				padding-left: 14px
-			    height: 26px
-			    line-height: 26px
-			    border-left: 2px solid #d9dde1
-			    font-size: 12px
-			    color: rgb(147,153,159)
-			    background: #f3f5f7
+				height: 26px
+				line-height: 26px
+				border-left: 2px solid #d9dde1
+				font-size: 12px
+				color: rgb(147,153,159)
+				background: #f3f5f7
 			.food-item
 				display: flex
 				margin: 18px;
@@ -154,6 +204,7 @@
 						color: rgb(147,153,159)
 					.desc
 						margin-bottom: 8px
+						line-height: 12px
 					.extra
 						.count
 							margin-right: 12px
